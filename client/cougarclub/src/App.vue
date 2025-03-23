6f9f222bb1f6812b5344df952a8d2d7bc3305604
CesarOsorioP <osoriocesar024@gmail.com>
2025-03-23 13:30:44 -0500

    Landing page

--- client/cougarclub/src/App.vue
+++ client/cougarclub/src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppNavbar />
+    <HeroSection />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from "./components/AppNavbar.vue";
+import HeroSection from "./components/HeroSection.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppNavbar,
+    HeroSection,
+  },
+};
 </script>
 
 <style>
+/* Asegura que toda la pantalla se use correctamente */
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden; /* Bloquea el scroll en toda la página */
+}
+
+/* Ajusta el contenedor principal */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;  
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh; /* Ocupará exactamente el alto de la pantalla */
+  display: flex;
+  flex-direction: column;
+  overflow: hidden; /* Asegura que nada sobresalga */
 }
 </style>
+
