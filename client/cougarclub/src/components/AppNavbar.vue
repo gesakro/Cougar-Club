c8ca600577729233a2231737b238aa69da677a8f
CesarOsorioP <osoriocesar024@gmail.com>
2025-03-23 13:30:44 -0500

    Landing page

---
+++ client/cougarclub/src/components/AppNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">Cougar Club</div>
+    <div class="menu">
+      <button class="menu-button">Menu</button>
+      <button class="search-button">Search</button>
+      <button class="cart-button">🛒</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style src="@/assets/styles/navbar.css"></style>
