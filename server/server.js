09636f262385b297e0c99f3de63538c6e820dae9
Santiago Gesama Valencia <sgesama3@gmail.com>
2025-04-29 13:41:06 -0500

    Backend connected succesfuly to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
+// server.js
+require('dotenv').config(); // Carga tus variables de entorno
+const cors = require('cors'); // Importa el paquete cors
 const express = require('express');
+const connectDB = require('./config/db');
+
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Middleware para parsear JSON y formularios
+// Configura Cors para permitir solicitudes desde http://localhost:3000
+app.use(cors({
+    origin: "http://localhost:3000",
+  }));
+
+// Conecta a MongoDB Atlas
+connectDB();
+
+// Middleware para parsear JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Middleware de logging: imprime la fecha, método y ruta de cada petición
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} a ${req.path}`);
-  next();
+
+app.get('/', (req, res) => {
+  res.send('Backend Express funcionando');
 });
 
-// Middleware de manejo de errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Ocurrió un error en el servidor.' });
-});
-
-// Arranca el servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
