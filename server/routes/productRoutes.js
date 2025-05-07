3907b0a04c5f23dbb3027b1d1513a394c471d4d4
Santiago Gesama Valencia <sgesama3@gmail.com>
2025-05-07 04:43:03 -0500

    Firebase storage connected succesfuly, banner and perfil company image are rendering on firebase storage and mongodb, same as product picture. TO-DO: Fix companyManagement CSS. Pls

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,16 +1,22 @@
+// routes/productRoutes.js
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { verifyToken, authorizeRole, checkCompanyOwnership } = require('../middlewares/auth');
+const uploadController = require('../controllers/uploadController');
+const { verifyToken, authorizeRole } = require('../middlewares/auth');
 
-// Rutas básicas para productos con autenticación
-router.post('/', verifyToken, authorizeRole('Administrador', 'Gerente'), checkCompanyOwnership, productController.createProduct);
-router.get('/', productController.getProducts); // Acceso público para ver productos
+// Ruta para subir imagen del producto
+router.post(
+  '/upload-image',
+  uploadController.uploadProductImageMiddleware,
+  uploadController.uploadProductImage
+);
+
+// Resto de tus rutas de productos...
+router.post('/', verifyToken, authorizeRole('Administrador', 'Gerente'), productController.createProduct);
+router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
-router.put('/:id', verifyToken, authorizeRole('Administrador', 'Gerente'), checkCompanyOwnership, productController.updateProduct);
-router.delete('/:id', verifyToken, authorizeRole('Administrador', 'Gerente'), checkCompanyOwnership, productController.deleteProduct);
-
-// Ruta adicional para obtener productos por marca
-router.get('/brand/:marca_id', productController.getProductsByBrand);
+router.put('/:id', verifyToken, authorizeRole('Administrador', 'Gerente'), productController.updateProduct);
+router.delete('/:id', verifyToken, authorizeRole('Administrador', 'Gerente'), productController.deleteProduct);
 
 module.exports = router;
