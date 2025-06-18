abea47eec943b63444c602e82d9a398038a4692f
CesarOsorioP <osoriocesar024@gmail.com>
2025-06-18 01:50:37 -0500

    Server.js v2

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 // Definir los orígenes permitidos
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080', 'https://cougar-club-9spj.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080', 'https://cougar-club-cojsco-n21t.vercel.app/'];
 
 app.use(express.json());
 
